8ba094967c60b4b8967165052a335c58a674a6ac
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-02-08 17:22:23 -0500

    Create seeds for all tables

---
+++ server/db/seeds/allSeeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO users (
+  first_name,
+  password,
+  email,
+  phone_number
+) VALUES (
+  'Liso',
+  '123',
+  'liso@piso.com',
+  '1111111111'
+)
+
+
+INSERT INTO books (user_id, title, author) VALUES (1, 'A Thousant Splendid Suns', 'Khaled Hosseini');
+INSERT INTO books (user_id, title, author) VALUES (1, 'And The Mountains Echoed', 'Khaled Hosseini');
+INSERT INTO books (user_id, title, author) VALUES (1, 'The Outsiders', 'S.E Hinton');
+
+
+INSERT INTO alerts (book_id, due_date, notes, text, email, calendar) VALUES (1, '2023-02-26', 'This book was borrowed from the Liso Library', true, NULL, NULL);
+INSERT INTO alerts (book_id, due_date, notes, text, email, calendar) VALUES (1, '2023-02-26', 'This book was borrowed from the Liso Library', NULL, true, NULL);
+INSERT INTO alerts (book_id, due_date, notes, text, email, calendar) VALUES (1, '2023-02-26', 'This book was borrowed from the Liso Library', NULL, true, true);
