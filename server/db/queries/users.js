44a3c691affd8e30ce63d7136f20741a32a638f5
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-02-08 20:17:02 -0500

    Create users queries

---
+++ server/db/queries/users.js
@@ -0,0 +1,34 @@
+const db = require('../index');
+
+const getAllUsers = async () => {
+  const data = await db.query(
+    'SELECT * FROM users;');
+  return data.rows;
+};
+
+const getUserById = async (id) => {
+  const data = await db.query(
+    `SELECT * FROM users
+     WHERE id = $1;`, [id]);
+  return data.rows;
+};
+
+const addUser = (first_name, email, password, phone_number) => {
+  return db.query(
+    `INSERT INTO users (first_name, email, password, phone_number) 
+    VALUES ($1, $2, $3, $4) 
+    RETURNING *;`,
+    [first_name, email, password, phone_number]
+  );
+};
+
+const getUserByLoginDetails = async (email, password) => {
+  const data = db.query(
+    `SELECT * FROM users 
+    WHERE email = $1 AND password = $2;`,
+    [email, password]);
+  return data.rows;
+};
+
+
+module.exports = { getAllUsers, getUserById, addUser, getUserByLoginDetails };
