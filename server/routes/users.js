b2fb5be843a740621f98f4fe6c7eaeda7facabc6
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-02-08 21:36:31 -0500

    Add users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,9 +4,36 @@ const db = require('../db/queries/users');
 
 // Routes for api/users
 
-router.get('/', (req, res) => {
-  db.getAllUsers()
-    .then((data) => {
+router
+  .route('/')
+  .get((req, res) => {
+    db.getAllUsers()
+      .then(data => {
+        res.send(data);
+        res.status(204);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  })
+  .post((req, res) => {
+    db.addUser()
+      .then(() => {
+        res.status(204);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  });
+
+
+
+router.get('/:id', (req, res) => {
+  db.getUserById(req.params.id)
+    .then(data => {
       res.send(data);
+    })
+    .catch(error => {
+      console.error(error);
     });
 });
