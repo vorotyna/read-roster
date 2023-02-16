0b9555c865532af4f648751c8b02730c2cd45841
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-02-15 21:06:30 -0500

    Create Home component and build App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./home/Home";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
