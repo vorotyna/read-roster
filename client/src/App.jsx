85a350b4a2ba071cea1c1c15455c5b406472e120
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-02-15 23:23:22 -0500

    Ensure tailwindcss configurations work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./home/Home";
+import Navbar from "./navbar/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Navbar />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ client/src/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <section className="mt-14">
+        read<span className="mx-0">roster</span>
+      </section>
+    </nav>
+  );
+}
+
+export default Navbar;
