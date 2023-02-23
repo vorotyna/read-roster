bd4ac16f56fcdb36c2728245da04ef94343e8c6a
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-02-22 22:32:35 -0600

    Add cursor-pointer to each tab and logo

--- client/src/navbar/Navbar.jsx
+++ client/src/navbar/Navbar.jsx
@@ -4,18 +4,18 @@ function Navbar() {
   return (
     <nav className="flex flex-row mt-14 items-baseline justify-between">
       <section>
-        <h1 className="text-4xl">
+        <h1 className="text-4xl cursor-pointer">
           read<span className="mx-0 font-bold c text-[#ff293ef2]">roster</span>
         </h1>
       </section>
       <section className="flex flex-row">
-      <h2 className="text-2xl mr-20">
+      <h2 className="text-2xl mr-20 cursor-pointer">
           my books
         </h2>
-      <h2 className="text-2xl mr-20">
+      <h2 className="text-2xl mr-20 cursor-pointer">
           due dates
         </h2>
-        <h2 className="text-2xl mr-20 outline">
+        <h2 className="text-2xl mr-20 bg-[#ff293ef2] rounded-full px-8 cursor-pointer">
           login
         </h2>
       </section>
