a3c545fb0ad3e998e1d0719a15ec4cc1c38732fd
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-02-22 22:19:18 -0600

    Change logo colour, outline login button

--- client/src/navbar/Navbar.jsx
+++ client/src/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <nav className="flex flex-row mt-14 items-baseline justify-between">
       <section>
         <h1 className="text-4xl">
-          read<span className="mx-0 font-bold">roster</span>
+          read<span className="mx-0 font-bold c text-[#ff293ef2]">roster</span>
         </h1>
       </section>
       <section className="flex flex-row">
@@ -15,7 +15,7 @@ function Navbar() {
       <h2 className="text-2xl mr-20">
           due dates
         </h2>
-        <h2 className="text-2xl mr-20">
+        <h2 className="text-2xl mr-20 outline">
           login
         </h2>
       </section>
