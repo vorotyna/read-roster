848fac18fd548229d2d553822924ba948a68bedf
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-02-22 22:06:10 -0600

    Add login tab

--- client/src/navbar/Navbar.jsx
+++ client/src/navbar/Navbar.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav>
-      <section className="mt-14">
+    <nav className="flex flex-row mt-14 items-baseline">
+      <section>
         <h1 className="text-3xl">
           read<span className="mx-0 font-bold">roster</span>
         </h1>
+      </section>
+      <section>
+        <h2 className="text-2xl">
+          login
+        </h2>
       </section>
     </nav>
   );
