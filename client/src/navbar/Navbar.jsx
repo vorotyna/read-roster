f2ff38e24445142df9eaabdccf1c963abd5f6819
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-02-22 21:57:16 -0600

    Put logo in an h1 tag

--- client/src/navbar/Navbar.jsx
+++ client/src/navbar/Navbar.jsx
@@ -3,8 +3,10 @@ import React from "react";
 function Navbar() {
   return (
     <nav>
-      <section className="mt-14 text-3xl">
-        read<span className="mx-0 font-bold">roster</span>
+      <section className="mt-14">
+        <h1 className="text-3xl">
+          read<span className="mx-0 font-bold">roster</span>
+        </h1>
       </section>
     </nav>
   );
