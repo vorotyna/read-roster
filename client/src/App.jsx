9b98c243a67355156169729e9fe4239840bbdbfa
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-02-24 22:34:13 -0600

    Setup Home page to begin HTML

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./navbar/Navbar";
+import Home from "./home/Home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navbar />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
