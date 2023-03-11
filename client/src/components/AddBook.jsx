150461d1b1b464ac982bb47994d6be71f47914e8
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-03-11 11:02:05 -0800

    Add .prettierrc, useState for AddBook clicks

--- client/src/components/AddBook.jsx
+++ client/src/components/AddBook.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddBook() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <button className="bg-gray-100 p-32 text-2xl font-light hover:bg-gray-200">+</button>
-  )
+    <button
+      className="bg-gray-100 p-32 text-2xl font-light hover:bg-gray-200"
+      onClick={() => setOpen(!open)}
+    >
+      +
+    </button>
+  );
 }
 
-export default AddBook
+export default AddBook;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import AddBook from "../../components/AddBook";
+import Modal from "../../components/Modal";
 
 function Home() {
+  const [open, setOpen] = useState(false);
+
   return (
     <div>
       <Navbar />
-      <h1 className="px-8 py-5 text-lg">Keep track of borrowed library books by adding them to your book roster!</h1>
+      <h1 className="px-8 py-5 text-lg">
+        Keep track of borrowed library books by adding them to your book roster!
+      </h1>
+      {open ? <Modal /> : <></>}
       <div className="grid grid-cols-4 gap-4 overflow-y-auto">
-        <AddBook/>
+        <AddBook setOpen={setOpen} />
       </div>
     </div>
-  )
+  );
 }
 
 export default Home;
