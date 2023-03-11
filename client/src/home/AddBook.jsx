197a96c7d14e1b6ffb2f6b5e14572b5f54b4363c
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-03-11 10:19:50 -0800

    Add hover state

--- client/src/home/AddBook.jsx
+++ client/src/home/AddBook.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function AddBook() {
   return (
-    <button className="bg-gray-100 p-32 text-2xl font-light">+</button>
+    <button className="bg-gray-100 p-32 text-2xl font-light hover:bg-gray-200">+</button>
   )
 }
 
