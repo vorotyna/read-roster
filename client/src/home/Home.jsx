4227b5e9063fa5d9078e226184d3131baf2559c1
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-03-11 09:59:03 -0800

    Add home description, add AddBook component

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -1,60 +1,16 @@
 import React from "react";
 import Navbar from "../navbar/Navbar";
+import AddBook from "./AddBook";
 
 function Home() {
   return (
-    <>
+    <div>
       <Navbar />
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-      <h1>HI</h1>
-    </>
+      <h1 className="px-8 py-5 text-lg">Keep track of borrowed library books by adding them to your book roster!</h1>
+      <div className="grid grid-columns-4 grid-flow-col gap-4">
+        <AddBook/>
+      </div>
+    </div>
   )
 }
 
