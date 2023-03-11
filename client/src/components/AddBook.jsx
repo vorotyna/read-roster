a40cc25972bc7eb80289f29417a32ca1e4209013
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-03-11 11:04:10 -0800

    Add border radius to AddBook component

--- client/src/components/AddBook.jsx
+++ client/src/components/AddBook.jsx
@@ -5,7 +5,7 @@ function AddBook() {
 
   return (
     <button
-      className="bg-gray-100 p-32 text-2xl font-light hover:bg-gray-200"
+      className="rounded-xl bg-gray-100 p-32 text-2xl font-light hover:bg-gray-200"
       onClick={() => setOpen(!open)}
     >
       +
