aedd7e47bdf7344caba5e1c41165148ead9fdcd2
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-03-11 11:35:33 -0800

    Install UI library for Modal, import Dialogue modal format into Modal component

--- client/src/components/AddBook.jsx
+++ client/src/components/AddBook.jsx
@@ -1,8 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
-function AddBook() {
-  const [open, setOpen] = useState(false);
-
+function AddBook({ setOpen, open }) {
   return (
     <button
       className="rounded-xl bg-gray-100 p-32 text-2xl font-light hover:bg-gray-200"
