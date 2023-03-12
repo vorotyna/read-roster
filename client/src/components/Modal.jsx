3634091440b31ab32508cf99a06b33be8b9865bc
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-03-12 13:24:22 -0700

    Blur background when Modal component appears

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 function Modal({ setOpen, open }) {
   return (
-    <div class="fixed inset-0 z-50 flex items-center justify-center">
+    <div class="fixed inset-0 z-20 flex items-center justify-center">
+      {/* <!-- Modal overlay --> */}
+      <div class="fixed inset-0 z-20 m-0 backdrop-blur"></div>
       {/* Modal container */}
-      <div class="flex h-auto w-3/5 flex-col justify-between rounded-lg bg-white p-8 shadow-lg">
+      <div class="z-20 flex h-auto w-3/5 flex-col justify-between rounded-lg bg-white p-8 shadow-lg">
         <div>
           <h2 class="mb-4 border-b-2 pb-3 text-xl font-medium">Add a book</h2>
           <div className="mb-6 flex flex-row items-start justify-around">
@@ -31,12 +33,12 @@ function Modal({ setOpen, open }) {
         </div>
         <div class="my-6 flex items-center justify-center">
           <button
-            class="mr-4 rounded bg-gray-500 px-4 py-2 text-white hover:bg-gray-600"
+            class="mr-4 rounded bg-gray-500 px-5 py-2 text-white hover:bg-gray-600"
             onClick={() => setOpen(!open)}
           >
             Cancel
           </button>
-          <button class="rounded bg-green-500 px-5 py-2 text-white hover:bg-green-600">
+          <button class="rounded bg-green-500 px-7 py-2 text-white hover:bg-green-600">
             Save
           </button>
         </div>
