425e732eb4e5c65831112d06eb1e7abb8bb96d1e
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-03-12 13:47:13 -0700

    Style Modal

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -2,43 +2,52 @@ import React from "react";
 
 function Modal({ setOpen, open }) {
   return (
-    <div class="fixed inset-0 z-20 flex items-center justify-center">
+    <div className="fixed inset-0 z-20 flex items-center justify-center">
       {/* <!-- Modal overlay --> */}
       <div class="fixed inset-0 z-20 m-0 backdrop-blur"></div>
       {/* Modal container */}
-      <div class="z-20 flex h-auto w-3/5 flex-col justify-between rounded-lg bg-white p-8 shadow-lg">
-        <div>
-          <h2 class="mb-4 border-b-2 pb-3 text-xl font-medium">Add a book</h2>
-          <div className="mb-6 flex flex-row items-start justify-around">
-            <section>
-              <form>
-                <label className="m-0">
+      <div className="z-20 flex h-auto w-3/5 flex-col justify-between rounded-lg bg-white p-8 shadow-lg">
+        <div className="mb-6">
+          <h2 className="mb-4 border-b-2 pb-3 text-xl font-medium">
+            Add a book
+          </h2>
+          <div className="flex flex-col sm:flex-row">
+            <section className="ml-0 w-full sm:w-1/2">
+              <form className="m-0">
+                <label className="m-0 block">
                   Title:
-                  <input type="text" name="title" />
+                  <input type="text" name="title" className="w-full rounded" />
                 </label>
-                <label className="m-0">
+                <label className="m-0 block">
                   Author:
-                  <input type="text" name="author" />
+                  <input type="text" name="author" className="w-full rounded" />
                 </label>
-                <label className="m-0">
+                <label className="m-0 block">
                   Library location:
-                  <input type="text" name="library" />
+                  <input
+                    type="text"
+                    name="library"
+                    className="w-full rounded"
+                  />
                 </label>
               </form>
             </section>
-            <section className="m-0 mt-1">
-              <input type="file" />
+            <section className="m-0 mt-4 w-full sm:mt-0 sm:w-1/2">
+              <label className="m-0 block">
+                Upload a photo:
+                <input type="file" className="w-full" />
+              </label>
             </section>
           </div>
         </div>
-        <div class="my-6 flex items-center justify-center">
+        <div className="my-6 flex items-center justify-center">
           <button
-            class="mr-4 rounded bg-gray-500 px-5 py-2 text-white hover:bg-gray-600"
+            className="mr-4 rounded bg-gray-500 px-5 py-2 text-white hover:bg-gray-600"
             onClick={() => setOpen(!open)}
           >
             Cancel
           </button>
-          <button class="rounded bg-green-500 px-7 py-2 text-white hover:bg-green-600">
+          <button className="rounded bg-blue-500 px-7 py-2 text-white hover:bg-blue-600">
             Save
           </button>
         </div>
