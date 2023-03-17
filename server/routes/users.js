5fa4ff2f1f9298fda84e93258dd1e5164e635fcf
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-03-17 08:59:16 -0700

    Update route

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,34 +4,32 @@ const db = require('../db/queries/users');
 
 // Routes for api/users
 
-router
-  .route('/')
-  .get((req, res) => {
-    db.getAllUsers()
-      .then(data => {
-        res.send(data);
-        res.status(204).send('All users retrieved');
-      })
-      .catch(error => {
-        res.status(500).send(error);
-      });
-  })
-  .post((req, res) => {
-    db.addUser()
-      .then(() => {
-        res.status(204).send('New user added');
-      })
-      .catch(error => {
-        res.status(500).send(error);
+router.get('/', (req, res) => {
+  db.getAllUsers()
+    .then(data => {
+      res.status(204).send(data);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    });
+});
 
-      });
-  });
+
+router.post('/new', (req, res) => {
+  db.addUser()
+    .then(() => {
+      res.status(204).send(data);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+
+    });
+});
 
 router.get('/:id', (req, res) => {
   db.getUserById(req.params.id)
     .then(data => {
-      res.send(data);
-      res.status(204).send('User retrieved by id');
+      res.status(204).send(data);
     })
     .catch(error => {
       res.status(500).send(error);
