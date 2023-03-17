fd722a4ab3a970456e59397796c17134d1f7b8cd
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-03-17 09:20:50 -0700

    Add comments to code, create handleRender function

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -6,11 +6,15 @@ import useBook from "../../hooks/useBook";
 import axios from "axios";
 
 function Home() {
+  // Set state to open or close the Modal
   const [open, setOpen] = useState(false);
+  // Set state to post the new book to DB
   const [isSave, setIsSave] = useState(false);
 
+  // Custom hook which handles the new book's state
   const { book, onBookChange, saveBook } = useBook();
 
+  // Props object that gets passed in to Modal
   let props = {
     book,
     onBookChange,
@@ -21,6 +25,7 @@ function Home() {
     setIsSave,
   };
 
+  // Function that makes a post request to save the book when none of the input fields are empty
   const handleSave = async () => {
     const isNotNull = Object.values(book).every(
       (param) => param !== null && param !== ""
@@ -28,13 +33,7 @@ function Home() {
 
     if (isNotNull) {
       try {
-        const saveSuccess = await axios.post(
-          "http://localhost:8001/api/books/new",
-          book
-        );
-        if (saveSuccess.data.success === true) {
-          console.log("SUCCESS-ADDED");
-        }
+        await axios.post("http://localhost:8001/api/books/new", book);
       } catch (error) {
         console.error(error);
       }
@@ -43,10 +42,22 @@ function Home() {
     }
   };
 
+  // Function the makes a get request when the page mounts and when isSave changes
+  const handleRender = async () => {
+    try {
+      await axios.get("http://localhost:8001/api/books");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // useEffect that if isSave is true, will handleSave() and then handleRender(). Otherwise, just handleRender()
   useEffect(() => {
     if (isSave) {
       handleSave();
     }
+
+    handleRender();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isSave]);
 
