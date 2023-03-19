4d9712caf14d165ccbf9c042131314c4583c2b32
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-03-19 10:36:42 -0700

    Fix z-index on navbar, add blurred background on ExistingBook when viewing alerts

--- client/src/components/existingBook.jsx
+++ client/src/components/existingBook.jsx
@@ -11,40 +11,48 @@ function ExistingBook({ book, index }) {
         onMouseLeave={() => setFlip(false)}
       >
         {flip ? (
-          <div className="m-0 flex h-full w-full flex-col items-center justify-around rounded-xl bg-gray-100">
-            <h1 className="mt-4 font-semibold">Current alert preferences:</h1>
-            <div className="m-6 my-0 px-6">
-              <label className="m-0 mb-2 flex cursor-pointer items-center">
-                <div className="relative m-0 ">
-                  <input type="checkbox" className="sr-only" />
-                  <div
-                    id="email-toggle"
-                    className="ml-0 mr-2 h-8 w-12 rounded-full bg-gray-500"
-                  ></div>
-                  <div className="dot absolute left-1 top-1 ml-0 h-6 w-6 rounded-full bg-white transition"></div>
-                </div>
-                <div className="m-0 mr-2">Email</div>
-              </label>
-              <label className="m-0 mb-2 flex cursor-pointer items-center ">
-                <div className="relative m-0 ">
-                  <input type="checkbox" className="sr-only" />
-                  <div className="ml-0 mr-2 h-8 w-12 rounded-full bg-gray-500"></div>
-                  <div className="dot absolute left-1 top-1 ml-0 h-6 w-6 rounded-full bg-white transition"></div>
-                </div>
-                <div className="m-0 mr-2">SMS</div>
-              </label>
-              <label className="m-0 mb-2 flex cursor-pointer items-center ">
-                <div className="relative m-0 ">
-                  <input type="checkbox" className="sr-only" />
-                  <div className="ml-0 mr-2 h-8 w-12 rounded-full bg-gray-500"></div>
-                  <div className="dot absolute left-1 top-1 ml-0 h-6 w-6 rounded-full bg-white transition"></div>
-                </div>
-                <div className="m-0 mr-2">Calendar</div>
-              </label>
+          <div className="relative m-0 flex h-full w-full flex-col items-center justify-around rounded-xl bg-cover bg-center ">
+            <img
+              src={book.photo}
+              key={index}
+              alt="cover"
+              className=" absolute z-0 m-0 h-72 w-64 rounded-xl object-cover blur-sm"
+            ></img>
+            <div className="relative m-0 flex h-full w-full flex-col items-center justify-around rounded-xl bg-cover bg-center">
+              <h1 className="mt-4 font-semibold">Current alert preferences:</h1>
+              <div className="m-6 my-0 px-6">
+                <label className="m-0 mb-2 flex cursor-pointer items-center">
+                  <div className="relative m-0 ">
+                    <input type="checkbox" className="sr-only" />
+                    <div
+                      id="email-toggle"
+                      className="ml-0 mr-2 h-8 w-12 rounded-full bg-gray-500"
+                    ></div>
+                    <div className="dot absolute left-1 top-1 ml-0 h-6 w-6 rounded-full bg-white transition"></div>
+                  </div>
+                  <div className="m-0 mr-2">Email</div>
+                </label>
+                <label className="m-0 mb-2 flex cursor-pointer items-center ">
+                  <div className="relative m-0 ">
+                    <input type="checkbox" className="sr-only" />
+                    <div className="ml-0 mr-2 h-8 w-12 rounded-full bg-gray-500"></div>
+                    <div className="dot absolute left-1 top-1 ml-0 h-6 w-6 rounded-full bg-white transition"></div>
+                  </div>
+                  <div className="m-0 mr-2">SMS</div>
+                </label>
+                <label className="m-0 mb-2 flex cursor-pointer items-center ">
+                  <div className="relative m-0 ">
+                    <input type="checkbox" className="sr-only" />
+                    <div className="ml-0 mr-2 h-8 w-12 rounded-full bg-gray-500"></div>
+                    <div className="dot absolute left-1 top-1 ml-0 h-6 w-6 rounded-full bg-white transition"></div>
+                  </div>
+                  <div className="m-0 mr-2">Calendar</div>
+                </label>
+              </div>
+              <button className="mx-auto mb-6 w-3/5 items-center rounded-full bg-[#ff293ef2] py-1 text-center">
+                delete book
+              </button>
             </div>
-            <button className="mx-auto mb-6 w-3/5 items-center rounded-full bg-[#ff293ef2] py-1 text-center">
-              delete book
-            </button>
           </div>
         ) : (
           <img
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,20 +3,16 @@ import React from "react";
 function Navbar() {
   return (
     // Position: Sticky needs to be revised to position:fixed, but currently I cannot get it to work properly
-    <nav className="flex flex-row items-baseline justify-between border-b-2 pb-4 sticky top-0 m-0 bg-white pt-10">
+    <nav className="sticky top-0 z-50 m-0 flex flex-row items-baseline justify-between border-b-2 bg-white pb-4 pt-10">
       <section className="">
-        <h1 className="text-4xl cursor-pointer">
-          read<span className="mx-0 font-bold c text-[#ff293ef2]">roster</span>
+        <h1 className="cursor-pointer text-4xl">
+          read<span className="c mx-0 font-bold text-[#ff293ef2]">roster</span>
         </h1>
       </section>
       <section className="flex flex-row items-center">
-      <h2 className="text-2xl mr-20 cursor-pointer">
-          my books
-        </h2>
-      <h2 className="text-2xl mr-20 cursor-pointer">
-          due dates
-        </h2>
-        <h2 className="text-2xl bg-[#ff293ef2] rounded-full px-8 cursor-pointer mr-0 py-1">
+        <h2 className="mr-20 cursor-pointer text-2xl">my books</h2>
+        <h2 className="mr-20 cursor-pointer text-2xl">due dates</h2>
+        <h2 className="mr-0 cursor-pointer rounded-full bg-[#ff293ef2] px-8 py-1 text-2xl">
           login
         </h2>
       </section>
