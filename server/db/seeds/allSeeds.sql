af0a89ef185b2eb962dd70a8641f060a506d6348
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-03-20 09:50:19 -0700

    Fix typom in seed

--- server/db/seeds/allSeeds.sql
+++ server/db/seeds/allSeeds.sql
@@ -11,7 +11,7 @@ INSERT INTO users (
 );
 
 
-INSERT INTO books (user_id, title, author, location, photo) VALUES (1, 'A Thousant Splendid Suns', 'Khaled Hosseini', 'Liso Library', 'https://productimages.worldofbooks.com/074758589X.jpg');
+INSERT INTO books (user_id, title, author, location, photo) VALUES (1, 'A Thousand Splendid Suns', 'Khaled Hosseini', 'Liso Library', 'https://productimages.worldofbooks.com/074758589X.jpg');
 INSERT INTO books (user_id, title, author, location, photo) VALUES (1, 'And The Mountains Echoed', 'Khaled Hosseini', 'Liso Library', 'https://m.media-amazon.com/images/I/51gXMIZ8sVL._AC_UF1000,1000_QL80_.jpg');
 INSERT INTO books (user_id, title, author, location, photo) VALUES (1, 'The Outsiders', 'S.E Hinton', 'Liso Library', 'https://interactive.wttw.com/sites/default/files/the-outsiders@2x.jpg');
 
