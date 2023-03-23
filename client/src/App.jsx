2aca8c3d3af2b0cf5a70354eabb1ee0efd580c67
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-03-23 09:45:00 -0700

    Create Login page and react-routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
