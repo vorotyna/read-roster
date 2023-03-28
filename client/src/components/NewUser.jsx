ab9622667acf69ef93e3dff7b2efca5017b9b893
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-03-28 16:18:06 -0700

    Create NewUser template

--- client/src/components/NewUser.jsx
+++ client/src/components/NewUser.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import PhoneInput from "react-phone-input-auto-format";
 
 function NewUser() {
-  return <div>NewUser</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <div className="w-1/2 translate-y-12 rounded-lg py-10 px-2 shadow">
+        <section>
+          <h2 className="text-center text-2xl	font-medium">Create account</h2>
+          <p className="mb-8 text-center">
+            Never miss an overdue library book again
+          </p>
+        </section>
+        <section className="flex flex-col items-center">
+          <form className="flex w-1/2 flex-col">
+            <input
+              type="text"
+              className="mb-2 rounded-full"
+              placeholder="Name *"
+              required
+            />
+            <input
+              type="text"
+              className="mb-2 rounded-full"
+              placeholder="Email *"
+              required
+            />
+            <PhoneInput
+              className="mb-2 rounded-full"
+              placeholder="Phone number *"
+              required
+            />
+            <input
+              type="password"
+              className="mb-8 rounded-full"
+              placeholder="Password *"
+              required
+            />
+          </form>
+        </section>
+        <section className="flex flex-row items-end justify-center">
+          <button className="rounded-full bg-[#ff293ef2] px-10 py-2 text-white hover:bg-[#ff293eb6]">
+            register
+          </button>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default NewUser;
