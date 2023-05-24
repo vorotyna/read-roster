3888bac8b911540ad622bb68408a0952411e855c
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2023-05-24 12:26:53 -0400

    Remove seeds

--- server/db/seeds/allSeeds.sql
+++ server/db/seeds/allSeeds.sql
@@ -7,18 +7,7 @@ INSERT INTO users (
   'Liso',
   '123',
   'alisa.vorotyn@gmail.com',
-  '+16506648860'
-);
-INSERT INTO users (
-  first_name,
-  password,
-  email,
-  phone_number
-) VALUES (
-  'Luko',
-  '123',
-  'luka.mircetic@gmail.com',
-  '+16503333655'
+  '+16472379202'
 );
 
 
@@ -28,7 +17,6 @@ INSERT INTO books (user_id, title, author, location, photo) VALUES (1, 'It Ends 
 INSERT INTO books (user_id, title, author, location, photo) VALUES (1, 'Nosotros En La Luna', 'Alice Kellen', 'Liso Library', 'https://prodimage.images-bn.com/pimages/9786070774249_p0_v1_s1200x630.jpg');
 INSERT INTO books (user_id, title, author, location, photo) VALUES (1, 'It Starts With Us', 'Colleen Hoover', 'Liso Library', 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/71PNGYHykrL._AC_UF1000,1000_QL80_.jpg');
 INSERT INTO books (user_id, title, author, location, photo) VALUES (1, 'Where the Crawdads Sing', 'Delia Owens', 'Liso Library', 'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81HA6TJ5K-L._AC_UF1000,1000_QL80_.jpg');
-INSERT INTO books (user_id, title, author, location, photo) VALUES (2, 'A Thousand Splendid Suns', 'Khaled Hosseini', 'Luko Library', 'https://productimages.worldofbooks.com/074758589X.jpg');
 
 
 INSERT INTO alerts (book_id, user_id, due_date, alert_time, SMS, email) VALUES (1, 1, '2030-05-01', '2030-04-10T22:35', TRUE, FALSE);
@@ -37,4 +25,3 @@ INSERT INTO alerts (book_id, user_id, due_date, alert_time, SMS, email) VALUES (
 INSERT INTO alerts (book_id, user_id, due_date, alert_time, SMS, email) VALUES (4, 1, '2030-05-21', '2030-05-10T22:35', FALSE, FALSE);
 INSERT INTO alerts (book_id, user_id, due_date, alert_time, SMS, email) VALUES (5, 1, '2030-04-28', '2030-05-10T22:35', FALSE, TRUE);
 INSERT INTO alerts (book_id, user_id, due_date, alert_time, SMS, email) VALUES (6, 1, '2030-04-20', '2030-05-10T22:35', TRUE, TRUE);
-INSERT INTO alerts (book_id, user_id, due_date, alert_time, SMS, email) VALUES (7, 2, '2030-04-29', '2030-05-10T22:35', FALSE, FALSE);
